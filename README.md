18b91629a77062aa8da5872fb8d67c53e625c995
wwwacedo <eng.marianammacedo@gmail.com>
2024-06-08 21:58:59 -0300

    fourth block

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [Code Connect](https://cursos.alura.com.br/course/next-js-conheca-framework-react)
 
-1. Subindo `json-server` na porta `3042`:
+1. Subindo [`json-server`](https://www.npmjs.com/package/json-server) na porta `3042`:
 ```bash
 npm i -g json-server@1.0.0-alpha.22
 json-server posts.json -p 3042
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import styles from "./page.module.css";
+import Link from "next/link";
 
-async function getAllPost() {
-	const response = await fetch('http://localhost:3042/posts')
+async function getAllPost(page) {
+	const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
 	if (!response.ok) {
 		logger.error('Ops, alguma coisa ocorreu mal.');
 		return [];
@@ -12,15 +13,20 @@ async function getAllPost() {
 	return await response.json();
 }
 
-export default async function Home() {
-	const posts = await getAllPost();
+export default async function Home({ searchParams }) {
+	const currentPage = searchParams?.page || 1;
+	const { data: posts, prev, next } = await getAllPost(currentPage);
 	return (
 		<main className={styles.grid}>
 			{
 				posts.map(post => (
 					<CardPost key={post.id} post={post} />
-				)) 
+				))
 			}
+			<div className={styles.links}>
+				{prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+				{next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+			</div>
 		</main>
 	);
 }
